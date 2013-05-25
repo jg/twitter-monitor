fc223a455993e30e864e070e78f1718b4db6b818
Juliusz Gonera <juliusz.gonera@gmail.com>
2013-05-25 13:34:42 +0200

    Add basic tweet parsing

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,28 @@
 package org.jg.twittermonitor
 
+import org.json.simple
+import org.json.simple._
+import scala.collection.JavaConversions._
+import scala.collection.breakOut
+import scala.collection.IndexedSeq
+
+object Tweet {
+  def fromJSON(json: String): Option[Tweet] = JSONValue.parse(json) match {
+    case obj: JSONObject => {
+      val keys = List("id", "text")
+      if (keys.forall(obj.containsKey(_))) {
+        val id = obj.get("id_str").asInstanceOf[String]
+        val text = obj.get("text").asInstanceOf[String]
+        Some(new Tweet(id, text))
+      } else None
+    }
+    case _ => None
+  }
+}
+
+class Tweet(val id: String, val text: String)
+
+// TODO: handle failure (tweets parseErrors)
 class TwitterClient(oauthConsumerKey: String,
                     oauthConsumerSecret: String,
                     oauthToken: String,
@@ -8,21 +31,35 @@ class TwitterClient(oauthConsumerKey: String,
 
   val homeTimelineUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
 
-  class Tweet
+  def homeTimeline(headers: Map[String, String]): Seq[Tweet] =
+    homeTimelineRaw(headers).flatMap(Tweet.fromJSON(_))
+
+  // Returns list of tweets in JSON
+  private def homeTimelineRaw(headers: Map[String, String]): Seq[String] = {
+    val response = get(homeTimelineUrl, headers)
+    val jsonArray: JSONArray = JSONValue.parse(response).asInstanceOf[JSONArray]
+    (for (i <- 0 to jsonArray.size-1)
+      yield jsonArray.get(i).asInstanceOf[JSONObject].toJSONString())(breakOut)
+  }
+
 
   // currently handles only GET requests
-  def request(method: String, url: String, headers: Map[String, String]): String = {
+  private def oauthRequest(method: String, url: String, headers: Map[String, String]): String = {
     val c = new HttpClient()
     val headerString = oauth.headerString(method, url, headers)
     val query = headers.map{case (k,v) => k + "=" + v}.mkString("&")
 
     c.get(url + "?" + query, Map[String, String]("Authorization" -> headerString) ++ headers)
   }
+
+  private def get(url: String, headers: Map[String, String]): String =
+    oauthRequest("GET", url, headers)
+
+}
 }
 object Main {
   import scalax.io._
 
-  val homeTimelineUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
 
   val apiTokens = new ApiTokens("config/api_tokens.yml")
   val client: TwitterClient = new TwitterClient(
@@ -32,17 +69,6 @@ object Main {
     apiTokens.get("oauthTokenSecret"))
 
   def main(args: Array[String]) = {
-    val headers = Map("count" -> "1")
-    val tweet = client.request("GET", homeTimelineUrl, headers)
-    val output: Output = Resource.fromFile("/home/jg/tweet.txt")
-    output.write(tweet)(Codec.UTF8)
-
-  /*
-    println(apiTokens.get("oauthConsumerKey"))
-    println(apiTokens.get("oauthConsumerSecret"))
-    println(apiTokens.get("oauthToken"))
-    println(apiTokens.get("oauthTokenSecret"))
-  */
   }
 
 }
--- src/test/scala/main.scala
+++ src/test/scala/main.scala
@@ -6,6 +6,18 @@ import org.jg.twittermonitor._
 class OAuthSuite extends FunSpec with ShouldMatchers {
   // example from https://dev.twitter.com/docs/auth/authorizing-request
 
+  describe("Tweet") {
+    val tweetJSON = """{"created_at":"Sun May 19 04:48:23 +0000 2013","id":335980230080622592,"id_str":"335980230080622592","text":"RT @cdixon: Autodesk buying Tinkercad and making it free is a win for users http:\/\/t.co\/ikgW8VPZqQ","source":"\u003ca href=\"http:\/\/www.linkedin.com\/\" rel=\"nofollow\"\u003eLinkedIn\u003c\/a\u003e","truncated":false,"in_reply_to_status_id":null,"in_reply_to_status_id_str":null,"in_reply_to_user_id":null,"in_reply_to_user_id_str":null,"in_reply_to_screen_name":null,"user":{"id":14344469,"id_str":"14344469","name":"Peter Skomoroch","screen_name":"peteskomoroch","location":"Silicon Valley","description":"My mission is to create intelligent systems that help people make better decisions. Principal Data Scientist @LinkedIn. Machine Learning, Hadoop, Big Data.","url":"http:\/\/t.co\/coIHrIxtTb","entities":{"url":{"urls":[{"url":"http:\/\/t.co\/coIHrIxtTb","expanded_url":"http:\/\/datawrangling.com","display_url":"datawrangling.com","indices":[0,22]}]},"description":{"urls":[]}},"protected":false,"followers_count":12321,"friends_count":902,"listed_count":858,"created_at":"Wed Apr 09 19:05:55 +0000 2008","favourites_count":1694,"utc_offset":-28800,"time_zone":"Pacific Time (US & Canada)","geo_enabled":true,"verified":false,"statuses_count":9316,"lang":"en","contributors_enabled":false,"is_translator":false,"profile_background_color":"1A1B1F","profile_background_image_url":"http:\/\/a0.twimg.com\/profile_background_images\/4230724\/Quantumfoam.jpg","profile_background_image_url_https":"https:\/\/si0.twimg.com\/profile_background_images\/4230724\/Quantumfoam.jpg","profile_background_tile":true,"profile_image_url":"http:\/\/a0.twimg.com\/profile_images\/3276454686\/8f8493dfc040e56ef7ff8f59f9474774_normal.jpeg","profile_image_url_https":"https:\/\/si0.twimg.com\/profile_images\/3276454686\/8f8493dfc040e56ef7ff8f59f9474774_normal.jpeg","profile_banner_url":"https:\/\/pbs.twimg.com\/profile_banners\/14344469\/1357265856","profile_link_color":"2FC2EF","profile_sidebar_border_color":"181A1E","profile_sidebar_fill_color":"252429","profile_text_color":"666666","profile_use_background_image":true,"default_profile":false,"default_profile_image":false,"following":true,"follow_request_sent":null,"notifications":null},"geo":null,"coordinates":null,"place":null,"contributors":null,"retweet_count":0,"favorite_count":0,"entities":{"hashtags":[],"symbols":[],"urls":[{"url":"http:\/\/t.co\/ikgW8VPZqQ","expanded_url":"http:\/\/blog.tinkercad.com\/2013\/05\/18\/autodesk_tinkercad\/","display_url":"blog.tinkercad.com\/2013\/05\/18\/aut\u2026","indices":[76,98]}],"user_mentions":[{"screen_name":"cdixon","name":"chris dixon","id":2529971,"id_str":"2529971","indices":[3,10]}]},"favorited":false,"retweeted":false,"possibly_sensitive":false,"lang":"en"}"""
+
+    describe("fromJSON") {
+      val tweetOpt = Tweet.fromJSON(tweetJSON)
+      for (tweet <- tweetOpt) {
+        tweet.id should equal("335980230080622592")
+        tweet.text should equal("""RT @cdixon: Autodesk buying Tinkercad and making it free is a win for users http://t.co/ikgW8VPZqQ""")
+      }
+    }
+  }
+
   describe("headerString") {
     val oauthConsumerKey = "xvz1evFS4wEEPTGEFPHBog"
     val oauthToken = "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb"
